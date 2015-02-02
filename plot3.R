093b003808365b9a7eb7be4e02fbb3497acf9dcf
James Clarke <jamclar2@cisco.com>
2015-02-02 19:00:40 +0000

    Added plot3.R

---
+++ plot3.R
@@ -0,0 +1,6 @@
+d <- loadData()
+plot(d$DateTime,d$Sub_metering_1,type='l',xlab="",ylab="Energy sub metering")
+lines(d$DateTime,d$Sub_metering_2,col="red")
+lines(d$DateTime,d$Sub_metering_3,col="blue")
+legend('topright', names(d)[7:9] , 
+       lty=1, col=c('black', 'red', 'blue'), cex=.75)
